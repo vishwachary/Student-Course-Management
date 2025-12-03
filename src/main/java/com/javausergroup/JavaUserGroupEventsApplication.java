3509cbb804b25b618f47d9cb074792d4a3da4f90
unknown <vishwachary.varanasi@gmail.com>
2025-12-03 15:45:24 +0530

    rename

--- src/main/java/com/javausergroup/JavaUserGroupEventsApplication.java
+++ src/main/java/com/javausergroup/JavaUserGroupEventsApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.javausergroup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/javausergroup/VoucherManagementSystemApplicationTests.java
+++ src/test/java/com/javausergroup/VoucherManagementSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.javausergroup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
