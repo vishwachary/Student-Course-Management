383969d43f2506693a6eb70e6426475e8f9aae92
unknown <vishwachary.varanasi@gmail.com>
2025-12-04 11:16:35 +0530

    changed the projectname and course

--- src/main/java/com/javausergroup/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javausergroup.repository;
-
-import com.javausergroup.model.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Integer> {
-}
